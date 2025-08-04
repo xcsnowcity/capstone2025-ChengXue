bf514bfe3dd5122b45e54e40c8ca1dc47d553fef
Cheng <1094553181xxcc@gmail.com>
2025-08-04 20:40:37 +0100

    Remove previous year's related app code

--- Timeline-main/readMe.md
+++
@@ -1,2 +0,0 @@
-Timeline: DV Prototype App
- 
--- Timeline-main/src/Utils/Toast.ts
+++
@@ -1,5 +0,0 @@
-import { ToastAndroid } from 'react-native';
-
-export function toast(msg) {
-  ToastAndroid.showWithGravityAndOffset(msg, ToastAndroid.SHORT, ToastAndroid.CENTER, 0, 0);
-}
